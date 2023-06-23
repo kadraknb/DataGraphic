5b615482d2c4a769da51705aa00b65ed60667daf
kadraknb <kadrak02@hotmail.com>
2023-06-23 14:13:55 -0300

    STYLE: organizando o navBar e add sombra .

--- src/components/NavBar/index.vue
+++ src/components/NavBar/index.vue
@@ -10,23 +10,33 @@ export default {
     ButtonDefault,
     ButtonUser,
   },
-  // data() {
-  // return {
-  //   msg: 'Welcome to Your Vue.js App',
-  // };
-  // },
 };
 </script>
 
 <template>
-  <div>
-    <img src='' alt="Logo" />
-    <ButtonDarkMode />
-    <ButtonDefault />
-    <ButtonUser />
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-white custom-navbar">
+    <div class="navbar-brand d-flex align-items-center justify-content-center flex-grow-1">
+      <img src="" alt="Logo" />
+    </div>
+    <div class="navbar-collapse justify-content-end">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <ButtonDarkMode />
+        </li>
+        <li class="nav-item">
+          <ButtonDefault />
+        </li>
+        <li class="nav-item">
+          <ButtonUser />
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
-
+.custom-navbar {
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  height: 84px;
+}
 </style>
