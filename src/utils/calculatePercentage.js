eed474ebefa9491927319bab2baae285ca722e1b
kadraknb <kadrak02@hotmail.com>
2023-06-23 11:16:30 -0300

    FEAT: add ordanao na funcao calculatePercentage.

--- src/utils/calculatePercentage.js
+++ src/utils/calculatePercentage.js
@@ -1,15 +1,29 @@
-function calculatePercentage(data) {
-  const totalPercentage = data.reduce((total, product) => total + product.Percentage, 0);
+function calculatePercentage(dados) {
+  const parsedDados = dados.map(item => ({
+    ...item,
+    vendas: parseInt(item.vendas, 10),
+    porcentagem: parseInt(item.porcentagem, 10),
+  }));
 
-  if (totalPercentage !== 0) {
-    return data.map(product => ({
-      ...product,
-      CalculatedPercentage: (product.Percentage / totalPercentage) * 100,
-    }));
+  const somaPorcentagens = parsedDados.reduce(
+    (total, produto) => total + produto.porcentagem,
+    0,
+  );
+
+  if (somaPorcentagens !== 0) {
+    return parsedDados.map((produto) => {
+      const porcentagemCalculada = parseFloat(
+        (produto.porcentagem / somaPorcentagens) * 100,
+      ).toFixed(2);
+      return {
+        ...produto,
+        PorcentagemCalculada: Number(porcentagemCalculada),
+      };
+    }).sort((a, b) => b.PorcentagemCalculada - a.PorcentagemCalculada);
   }
-  return data.map(product => ({
-    ...product,
-    CalculatedPercentage: 0,
+  return parsedDados.map(produto => ({
+    ...produto,
+    PorcentagemCalculada: 0,
   }));
 }
 
