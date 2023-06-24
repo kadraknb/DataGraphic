6830ae0f48bf88b3310d31fff5a7ec8ca26ee259
kadraknb <kadrak02@hotmail.com>
2023-06-24 17:06:32 -0300

    STYLE: componete navbar.

--- src/components/NavBar/index.vue
+++ src/components/NavBar/index.vue
@@ -1,45 +1,65 @@
 <script>
-import ButtonDarkMode from '../ButtonDarkMode';
-import ButtonUser from '../User/ButtonUser';
-import ButtonDefault from '../ButtonDefault';
+import DarkMode from '../Buttons/DarkMode';
+import User from '../Buttons/User';
+import Default from '../Buttons/Default';
+import database from '../../images/database-solid.svg';
+import tableCells from '../../images/table-cells-large-solid.svg';
+import expandSolid from '../../images/expand-solid.svg';
 
 export default {
   name: 'NavBar',
   components: {
-    ButtonDarkMode,
-    ButtonDefault,
-    ButtonUser,
+    DarkMode,
+    Default,
+    User,
+
   },
+  data() {
+    return {
+      database,
+      tableCells,
+      expandSolid,
+    };
+  }
 };
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-white custom-navbar shadow-default">
+  <nav class="navbar-light bg-white custom-navbar shadow-default">
     <div></div>
-    <div class="navbar-brand d-flex align-items-center justify-content-center flex-grow-1">
+    <div class="">
       <img class="logo-navBar" src="../../images/Logo.png" alt="Logo" />
     </div>
-    <div class="navbar-collapse justify-content-end">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <ButtonDarkMode />
-        </li>
-        <li class="nav-item">
-          <ButtonDefault />
-        </li>
-        <li class="nav-item">
-          <ButtonUser />
-        </li>
-      </ul>
+    <div class="buttons-NavBar">
+      <DarkMode />
+      <!-- ajust add nome icon -->
+      <Default :icon="database" />
+      <Default :icon="tableCells" />
+      <Default :icon="expandSolid" />
+      <User />
     </div>
   </nav>
 </template>
 
 <style scoped>
+.buttons-NavBar {
+  border: solid red;
+  width: 300px;
+  padding-top: 5px;
+  margin-left: -311px;
+  display: flex;
+  flex-direction: row;
+}
+
 .custom-navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
   height: 67px;
 }
-.logo-navBar{
-  width: 60px;
+
+.logo-navBar {
+  width: 62px;
 }
 </style>
