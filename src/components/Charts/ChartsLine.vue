2859802d164cdb0c90d65e27cf60ca2ec7b21a5b
kadraknb <kadrak02@hotmail.com>
2023-06-24 21:48:14 -0300

    REFACTOR: colocando a config dos graficos em outra pasta.

--- src/components/Charts/ChartsLine.vue
+++ src/components/Charts/ChartsLine.vue
@@ -1,12 +1,7 @@
-<template>
-  <div class="shadow-default bg-white rounded custom-apexchart-area">
-    <div class="area">
-      <apexchart type="line" height="350" :options="chartOptions" :series="series" />
-    </div>
-  </div>
-</template>
 <script>
 import VueApexCharts from 'vue-apexcharts';
+import chartOptions from './config/line'
+
 
 export default {
   name: 'ChartsLine',
@@ -21,82 +16,40 @@ export default {
         data: this.faturamento.map(arr => arr.faturamentoMes),
       }],
       chartOptions: {
-        chart: {
-          height: 300,
-          type: 'line',
-          toolbar: { show: false },
-          animations: { enabled: false },
-          zoom: { enabled: false },
-        },
-        dataLabels: { enabled: false },
-        stroke: {
-          colors: '#4b4bf9',
-          curve: 'smooth',
-          width: 3,
-        },
-        markers: {
-          strokeColors: '#0000000',
-          colors: '#4b4bf9',
-          size: 5,
-          hover: {
-            size: 5,
-          },
-        },
-        title: {
-          margin: 14,
-          text: 'Total de vendas - Últimos 12 meses',
-          offsetX: 14,
-          offsetY: 16,
-          style: {
-            fontSize: '16px',
-            fontWeight: 'normal',
-            fontFamily: 'Nunito, sans-serif',
-            color: '#444444',
-          },
-        },
+        ...chartOptions,
         xaxis: {
-          type: 'datetime',
+          type: "datetime",
           categories: this.faturamento.map(arr => arr.data),
           tickAmount: this.faturamento.length,
           labels: {
             formatter(value, timestamp) {
               const date = new Date(timestamp);
-              const month = date.toLocaleString('default', { month: 'short' });
+              const month = date.toLocaleString("default", { month: "short" });
               const year = date.getFullYear();
               return `${month}/${year}`;
             },
             style: {
-              colors: '#444444',
-              fontSize: '10px',
-              fontFamily: 'Nunito, sans-serif',
+              colors: "#444444",
+              fontSize: "10px",
+              fontFamily: "Nunito, sans-serif"
             },
-          },
-        },
-        yaxis: {
-          tickAmount: 10,
-          labels: {
-            minWidth: 0,
-            maxWidth: 160,
-            decimalsInFloat: 4,
-            floating: true,
-            style: {
-              colors: '#707070',
-              fontSize: '10px',
-              fontFamily: 'Nunito, sans-serif',
-              fontWeight: 100,
-            },
-          },
-        },
-        tooltip: {
-          x: {
-            format: 'dd/MM/yy HH:mm',
           },
         },
       },
-    };
-  },
+    }
+  }
 };
+
 </script>
+
+<template>
+  <div class="shadow-default bg-white rounded custom-apexchart-area">
+    <div class="area">
+      <apexchart type="line" height="350" :options="chartOptions" :series="series" />
+    </div>
+  </div>
+</template>
+
 <style scoped>
 .custom-apexchart-area {
   height: 23.3rem;
---
+++ src/components/Charts/config/line.js
@@ -0,0 +1,57 @@
+const chartOptions = {
+  chart: {
+    height: 300,
+    type: "line",
+    toolbar: { show: false },
+    animations: { enabled: false },
+    zoom: { enabled: false }
+  },
+  dataLabels: { enabled: false },
+  stroke: {
+    colors: "#4b4bf9",
+    curve: "smooth",
+    width: 3
+  },
+  markers: {
+    strokeColors: "#0000000",
+    colors: "#4b4bf9",
+    size: 5,
+    hover: {
+      size: 5
+    }
+  },
+  title: {
+    margin: 14,
+    text: "Total de vendas - Últimos 12 meses",
+    offsetX: 14,
+    offsetY: 16,
+    style: {
+      fontSize: "16px",
+      fontWeight: "normal",
+      fontFamily: "Nunito, sans-serif",
+      color: "#444444"
+    }
+  },
+  yaxis: {
+    tickAmount: 10,
+    labels: {
+      minWidth: 0,
+      maxWidth: 160,
+      decimalsInFloat: 4,
+      floating: true,
+      style: {
+        colors: "#707070",
+        fontSize: "10px",
+        fontFamily: "Nunito, sans-serif",
+        fontWeight: 100
+      }
+    }
+  },
+  tooltip: {
+    x: {
+      format: "dd/MM/yy HH:mm"
+    }
+  }
+};
+
+export default chartOptions
